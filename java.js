1c43d5f1731cfcbc27adab7270431cba832f1cf7
xEliasParedes <Paredeselias7@gmail.com>
2024-04-09 20:19:08 -0400

    navbar con java script

---
+++ java.js
@@ -0,0 +1,15 @@
+let ubicacionPrincipal = window.pageYOffset;
+window.onscroll = function(){
+
+  
+    let Desplazamiento = window.pageYOffset;
+    if(ubicacionPrincipal >= Desplazamiento){
+        document.getElementById('navbar').style.top = '0px';
+
+    }
+    else{
+        document.getElementById('navbar').style.top = '-200px';
+        
+    }
+    ubicacionPrincipal = Desplazamiento;
+}
