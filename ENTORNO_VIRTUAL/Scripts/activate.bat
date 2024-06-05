47d9290c6768c5cf5e9d825757dfec82f6f89064
Vina del Mar <vina@duoc.acad>
2024-06-05 16:58:00 -0400

    transformacion a django

--- ENTORNO_VIRTUAL/Scripts/activate.bat
+++ ENTORNO_VIRTUAL/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\vina\Downloads\PruebaSemestral-main\ENTORNO_VIRTUAL
+set VIRTUAL_ENV=C:\Users\vina\Downloads\PruebaSemestral-Elias\ENTORNO_VIRTUAL
 
 if not defined PROMPT set PROMPT=$P$G
 
